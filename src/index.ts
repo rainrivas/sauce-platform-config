9a1d873f42224490648fbdffead848a6e7012419
Efrain 'Rain' Rivas <rainrivas@gmail.com>
2018-11-29 18:31:59 -0800

    Begin the type-scripi-fication

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { config } from 'dotenv';
+import platformConfigs from './platformConfigs'
+
+config(); // load environment variables
+platformConfigs();
+
+// sauce api-- https://YOUR_SAUCE_USERNAME:YOUR_SAUCE_ACCESS_KEY@saucelabs.com/rest/v1/users/YOUR_SAUCE_USERNAME
+// test configs -- https://wiki.saucelabs.com/display/DOCS/Test+Configuration+Options
+// required dcs -- https://wiki.saucelabs.com/display/DOCS/Desired+Capabilities+Required+for+Selenium+and+Appium+Tests
+// platform config tool -- https://wiki.saucelabs.com/display/DOCS/Platform+Configurator
---
+++ src/platformConfigs.ts
@@ -0,0 +1,82 @@
+export default function platformConfigs() {
+    interface slcaps {
+        seleniumVersion: string;
+        extendedDebugging?: boolean;
+        browserName?: string;
+        version?: number
+        platform?: string
+        tunnel?: string
+        name?: string
+        build?: string
+    }
+    if (process.env.SAUCE_USER === 'undefined' || process.env.SAUCE_AUTHKEY === 'undefined') {
+        throw new Error(`Sauce username and authkey must both be defined, ${process.env.SAUCE_USER} | ${process.env.SAUCE_AUTHKEY}`);
+    }
+    let host = `https://${process.env.SAUCE_USER}:${process.env.SAUCE_AUTHKEY}@ondemand.saucelabs.com:443/wd/hub`;
+    let slconfig = process.env.SAUCE_BROWSER.split('-');
+    
+    let caps = {
+        'seleniumVersion': '3.14.0',
+        'extendedDebugging': process.env.SAUCE_DEBUG,// fixed
+        // 'browserName':"" ,
+        // 'version':"" ,
+        // 'platform':"",
+        // 'tunnel':"" ,
+        // 'name':"" ,
+        // 'build':"" 
+    };
+
+    // Declare optional values now so we can use them later
+    let version = process.env.SAUCE_BROWSER_VERSION;
+    let platform = process.env.SAUCE_OS;
+    let tunnel = process.env.SAUCE_TUNNEL;
+
+    if ('firefox, chrome, safari'.includes(slconfig[2])) {
+        caps.browserName = slconfig[2];
+        if (platform === 'undefined' || platform.includes('Windows')) { platform = 'macOS 10.13' }
+    } else if ('ie'.includes(slconfig[2])) {
+        caps.browserName = 'internet explorer';
+        if (platform === 'undefined' || platform.includes('mac')) { platform = 'Windows 10' }
+    } else if ('edge'.includes(slconfig[2])) {
+        caps.browserName = 'MicrosoftEdge';
+        if (platform === 'undefined' || platform.includes('mac')) { platform = 'Windows 10' }
+    } else { // if no good browser name was found, just make it random
+        console.warn('Browser was not passed, we are creating a default browser os combo');
+        caps.browserName = 'firefox';
+        switch (Math.round(Math.random() * Math.max(4))) {
+            case 0:
+                caps.browserName = 'firefox';
+                version = 'latest';
+                break;
+            case 1:
+                caps.browserName = 'chrome';
+                version = 'latest';
+                break;
+            case 2:
+                caps.browserName = 'internet explorer';
+                if (platform === 'undefined' || platform.includes('mac')) { platform = 'Windows 10' }
+                version = 'latest';
+                break;
+            case 3:
+                caps.browserName = 'safari';
+                if (platform === 'undefined' || platform.includes('Windows')) { platform = 'macOS 10.13' }
+                version = 'latest';
+                break;
+            default:
+                caps.browserName = 'MicrosoftEdge';
+                if (platform === 'undefined' || platform.includes("mac")) { platform = 'Windows 10' }
+                version = 'latest';
+                break;
+        }
+    }
+
+    // assign optional caps their default values if none is defined
+    (version === 'undefined') ? caps.version = 'latest' : caps.version = version;
+    (platform === 'undefined') ? caps.platform = 'Windows 10' : caps.platform = platform;
+    (tunnel === 'undefined') ? caps.tunnel = '' : caps.tunnel = tunnel;
+
+    caps.name = process.env['TEST_NAME'];
+    caps.build = process.env['BUILD_NUMBER'];
+    console.info(`Custom SL driver caps: ${JSON.stringify(caps)}`);
+    return { "host": host, "caps": caps }
+}
